e1f9e2daee515a7fa5cb9333c509f1ff7d5ed194
Lohoskoi Loh <yana.grigorieva97@mail.ru>
2023-06-26 20:04:10 +0500

    finish

--- testF/testF/MainWindow.xaml.cs
+++ testF/testF/MainWindow.xaml.cs
@@ -28,6 +28,23 @@ namespace testF
         {
             CountLabel.Content = DataG.Items.Count + " из " + App.DBProd.ProdTest.Count();
         }
+        public void CheckCB()
+        {
+            if (DataG.Items.Count == 0)
+            {
+                MessageBox.Show("Нет товаров с такими параметрами!");
+            }
+        }
+        public void SetInfo(ProdTest a)
+        {
+            NameTB.Text = a.Name;
+            imgProd.Source = new BitmapImage(new Uri($"{a.ImgPath}", UriKind.Relative));
+            WoutDisTB.Text = a.Price.ToString();
+            WDisTB.Text =a.PriceWDis.ToString();
+            var bc = new BrushConverter();
+            panel.Background= (Brush)bc.ConvertFrom(a.Color);
+
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -41,21 +58,42 @@ namespace testF
             {
                 DataG.ItemsSource = App.DBProd.ProdTest.Where(a => a.Discount >= 0 && a.Discount <= 9.99).ToList();
                 CountAll();
+                CheckCB();
             }
             else if (CBSort1.SelectedIndex == 1)
             {
                 DataG.ItemsSource = App.DBProd.ProdTest.Where(a => a.Discount >= 10 && a.Discount <= 14.99).ToList();
                 CountAll();
+                CheckCB();
             }
             else if (CBSort1.SelectedIndex == 2)
             {
                 DataG.ItemsSource = App.DBProd.ProdTest.Where(a => a.Discount >= 15).ToList();
                 CountAll();
+                CheckCB();
             }
             else if (CBSort1.SelectedIndex == 3)
             {
-                Load_DB();
-                CountAll();
+                if (DataG.Items.Count != App.DBProd.ProdTest.Count())
+                {
+                    if (MessageBox.Show("Вы уверены что хотите сбросить выбор?", "?", MessageBoxButton.OKCancel, MessageBoxImage.Question) == MessageBoxResult.OK)
+                    {
+                        Load_DB();
+                        CountAll();
+                        CheckCB();
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    Load_DB();
+                    CountAll();
+                    CheckCB();
+                }
+                
             }
         }
 
@@ -79,6 +117,13 @@ namespace testF
                 DataG.ItemsSource = sorted;
                 CountAll();
             }
+
+        }
+
+        private void DataG_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            ProdTest SelectedProduct = DataG.SelectedItem as ProdTest;
+            SetInfo(SelectedProduct);
         }
     }
 }
